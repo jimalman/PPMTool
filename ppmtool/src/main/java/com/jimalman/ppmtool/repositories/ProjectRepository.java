849f0553e4f9a1ef4912c35c96b9f4a8cedbc818
Jim <jcalman9@gmail.com>
2019-01-11 14:27:41 -0500

    added mproject model

---
+++ ppmtool/src/main/java/com/jimalman/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,12 @@
+package com.jimalman.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jimalman.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+  @Override
+  Iterable<Project> findAllById(Iterable<Long> iterable);
+}
