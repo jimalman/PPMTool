6c1ec188ad7e06155a974b639c90bc9893dd9ad5
Jim <jcalman9@gmail.com>
2019-01-11 11:32:26 -0500

    first commit

---
+++ ppmtool/src/main/java/com/jimalman/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,14 @@
+package com.jimalman.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
+
