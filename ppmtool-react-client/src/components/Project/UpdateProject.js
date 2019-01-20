ee2efb27cac71be5df38e40bdabf67f8df907e4e
Jim <jcalman9@gmail.com>
2019-01-20 12:08:15 -0500

    update working

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -1,31 +1,106 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import classnames from "classnames";
-import { getProject } from "../../actions";
+
+import { getProject, createProject } from "../../actions";
+import ProjectForm from "./ProjectForm";
 
 class UpdateProject extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      id: "",
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      startDate: "",
+      endDate: ""
+    };
+  }
+
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getProject(id, this.props.history);
   }
 
+  componentWillReceiveProps(nextProps) {
+    const {
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      startDate,
+      endDate
+    } = nextProps.project;
+
+    this.setState({
+      id,
+      projectName,
+      projectIdentifier,
+      description,
+      startDate,
+      endDate
+    });
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const newProject = {
+      id: this.state.id,
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate
+    };
+    this.props.createProject(newProject, this.props.history);
+  };
+
   render() {
     return (
-      <div>
-        <h1>Update Project</h1>
+      <div className="project">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h5 className="display-4 text-center">Update Project form</h5>
+              <hr />
+              <ProjectForm
+                onChange={this.onChange}
+                onSubmit={this.onSubmit}
+                errors={this.props.errors}
+                projectName={this.state.projectName}
+                projectIdentifier={this.state.projectIdentifier}
+                description={this.state.description}
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 UpdateProject.propTypes = {
-  getProject: PropTypes.func.isRequired
+  getProject: PropTypes.func.isRequired,
+  createProject: PropTypes.func.isRequired,
+  project: PropTypes.object.isRequired
 };
 
-//mapStateToProps = state = {};
+const mapStateToProps = state => {
+  return {
+    errors: state.errors,
+    project: state.project.project
+  };
+};
 
 export default connect(
-  null,
-  { getProject }
+  mapStateToProps,
+  { getProject, createProject }
 )(UpdateProject);
