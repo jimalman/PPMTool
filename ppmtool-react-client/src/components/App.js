7f2aaa84125b908f62ec41c70eb63e82cf0b6237
Jim <jcalman9@gmail.com>
2019-01-27 20:30:09 -0500

    began working project task front end

--- ppmtool-react-client/src/components/App.js
+++ ppmtool-react-client/src/components/App.js
@@ -8,6 +8,8 @@ import Dashboard from "./Dashboard";
 import Header from "./Layout/Header";
 import AddProject from "./Project/AddProject";
 import UpdateProject from "./Project/UpdateProject";
+import ProjectBoard from "./ProjectBoard/ProjectBoard";
+import AddProjectTask from "./ProjectBoard/ProjectTasks/AddProjectTask";
 import store from "../store";
 
 export default class App extends Component {
@@ -20,6 +22,12 @@ export default class App extends Component {
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
           </div>
         </Router>
       </Provider>
