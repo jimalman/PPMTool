8dca2a918fbd47459161bc957f5994eb800f9849
Jim <jcalman9@gmail.com>
2019-02-15 21:15:48 -0500

    worked on Login form

--- ppmtool-react-client/src/components/App.js
+++ ppmtool-react-client/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "../securityUtils/setJWTToken";
 
 import "./App.css";
 import Dashboard from "./Dashboard";
@@ -15,6 +17,23 @@ import store from "../store";
 import Landing from "./Layout/Landing";
 import Register from "./UserManagement/Register";
 import Login from "./UserManagement/Login";
+import { SET_CURRENT_USER } from "../actions/types";
+
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decodedJWTToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decodedJWTToken
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decodedJWTToken.exp < currentTime) {
+    //window.location.href = "/";
+  }
+}
 
 export default class App extends Component {
   render() {
